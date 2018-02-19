653a6c2dc8d55089aeaa9f3983a09ff338fba4b6
jamesapdx <jim@jimpdx.com>
2018-02-18 18:11:38 -0800

    create pseudo code for main loop
    main loop is a char at a time comparison to the last results,
    write out a heat map (difference) for changed characters.

--- watchp.py
+++ watchp.py
@@ -12,26 +12,72 @@ def run_linux(cmd):
                 
 
 def main(stdscr):
-    pass
+
+    clear heat array
+    get term sizes
+    define history arry
+    new window, size to term sizes
+    
+    while True:
+        results = run linux command
+        clear screen
+
+        check terminal size
+            if not same
+                set new sizes
+                clear last result
+                clear heat array
+                resize window
+                
+        loop lines
+            loop columns
+                if now char != past char
+                    set hot
+                else if temp > 1
+                    set temp - 1
+
+            write char
+        refresh window
+        
+        save results to history array
+
+        get char input
+        user choices:
+            quit
+            pause(later)
+            unpause(later)
+        
+
+
+    term_lines, term_columns = 0,0
+    if term_lines, term_columns != curses.LINES, curses.COLS:
+        term_lines, term_columns = curses.LINES, curses.COLS
+
+    for line in range(term_lines - 1):
+        for column in range(term_columns - 1):
+
+
 
 
 stdscr = curses.initscr()
 curses.noecho()
 curses.cbreak()
+curses.curs_set(0)
+curses.start_color()
 stdscr.keypad(True)
 
-done = False
+def terminate_curses():
+    curses.echo()
+    curses.nocbreak()
+    curses.curs_set(1)
+    curses.endwin()
+
+
 try:
     main(stdscr)
 except:
-    curses.echo()
-    curses.nocbreak()
-    curses.endwin()
-    done = True
+    terminate_curses()
     raise
 
-if done is not True:
-    curses.echo()
-    curses.nocbreak()
-    curses.endwin()
-
+if curses.isendwin() is not True:
+    terminate_curses()
